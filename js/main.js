2c69074e03a081b38cd6a749a59fe99f92eaaf86
Jhymas20 <Jordanhymas24@gmail.com>
2024-05-07 22:50:23 -0700

    JavaScript portfolio section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*========== Mixitup Filter ==========*/
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item',
+    },
+    animation: {
+        duration: 300,
+    }
+});
