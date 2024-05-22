c49077380b4bed54efbfb763768baac4e8e17413
Jhymas20 <Jordanhymas24@gmail.com>
2024-05-21 21:55:26 -0700

    Blog Page Add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/*========== Active Link ==========*/
+const navlink = document.querySelectorAll('.nav__link');
+
+function activeLink() {
+    nav.forEach((a) => a.classList.remove('active-link'));
+    this.classList.add('active-link');
+}
+
+navlink.forEach((a) => a.addEventListener('click', activeLink));
 
 
 /*========== Mixitup Filter ==========*/
@@ -37,62 +46,6 @@ var testiSwiper = new Swiper(".testimonial__container", {
     keyboard: true,
   });
 
-
-/*========== Contact Form ==========*/
-const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-Message = document.getElementById('message'),
-contactMessage = document.getElementById('contact-message');
-
-const sendEmail = (e) => {
-  e.preventDefault();
-
-  // check if the field has a value
-  if(
-    contactName.value === '' || 
-    contactEmail.value === '' || 
-    Message.value === ''
-  ) 
-    {
-      // add and remove color 
-      contactMessage.classList.remove('color-light');
-      contactMessage.classList.add('color-dark');
-
-      //show message
-      contactMessage.textContent = 'Write all the input fields';
-    } else {
-      //serviceID - templateID - #form - publickey
-      emailjs
-        .sendForm(
-        'service_8zf4uar', 
-        'template_jjqzstb',
-        '#contact-form',
-        '7bSAmSL4a_v5xfnVZ'
-        )
-      .then(() => {
-        // show message and add color
-        contactMessage.classList.add('color-light');
-        contactMessage.textContent = 'Message sent ✅';
-
-        //remove message after 5 seconds
-        setTimeout(() => {
-          contactMessage.textContent = '';
-        }, 5000);
-      },
-      (error) => {
-        alert('OOPs! SOMETHING WHENT WRONG', error);
-      }
-    );
-    // clear the form
-    contactName.value = '';
-    contactEmail.value = '';
-    Message.value = '';
-
-  }
-};
-
-contactForm.addEventListener('submit', sendEmail);
 
 /*========== Scroll to the top ==========*/
   // Get the button:
