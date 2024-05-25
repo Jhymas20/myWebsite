aff5b5e62f9054c8fbee996148e5ba0abb46e465
Jhymas20 <Jordanhymas24@gmail.com>
2024-05-25 11:43:41 -0700

    Added Media Querys and added to server

--- js/common.js
+++ js/common.js
@@ -1,3 +1,24 @@
+/*========== Show Menu ==========*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+    /*========== Menu Show ==========*/
+    /* Validate if constant exists */
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu');
+        });
+    }
+
+        /*========== Hide/Close Show Menu ==========*/
+    /* Validate if constant exists */
+    if (navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+        });
+    }
+
 /*========== Background header ==========*/
 function scrollHeader() {
     const header = document.getElementById('header');
