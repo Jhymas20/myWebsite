1463c8c3e678e3d2570747225674318df2c13f0a
Jhymas20 <Jordanhymas24@gmail.com>
2024-05-26 16:18:29 -0700

    Added Settings to change the color

--- js/common.js
+++ js/common.js
@@ -19,6 +19,18 @@ const navMenu = document.getElementById('nav-menu'),
         });
     }
 
+/*========== Remove Menu Mobile ==========*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // when we click on each link we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+
 /*========== Background header ==========*/
 function scrollHeader() {
     const header = document.getElementById('header');
@@ -84,3 +96,34 @@ const sendEmail = (e) => {
 };
 
 contactForm.addEventListener('submit', sendEmail);
+
+/*========== Styler Switcher ==========*/
+const styleSwitcherToggler = document.querySelector('.style__switcher-toggler'),
+  styleSwitcher = document.querySelector('.style__switcher');
+
+styleSwitcherToggler.addEventListener('click', () => {
+  styleSwitcher.classList.toggle('open');
+});
+
+// hide switcher on scroll
+
+window.addEventListener('scroll', () => {
+  if(styleSwitcher.classList.contains('open')) {
+    styleSwitcher.classList.remove('open');
+  }
+});
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if(color === style.getAttribute('title')) {
+      style.removeAttribute('disabled');
+    } else {
+      style.setAttribute('disabled', 'true');
+    }
+  });
+}
+
+
+
